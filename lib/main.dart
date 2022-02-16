0023fc1288dfd35d43e2e006acb2df515616836d
SuryaAndroid <magnetcreativespub@gmail.com>
2022-02-16 15:24:30 +0530

    coimbatore changed

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class _MyAppState extends State<MyApp> {
         },
         child: Scaffold(
           appBar: AppBar(
-            title: Text('Mindmade'),
+            title: Text('Coimbatore'),
           ),
         ),
       ),
